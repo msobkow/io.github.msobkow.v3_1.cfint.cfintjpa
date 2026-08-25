1cb13af872fca1d517e4b14e8d6b0012528f0e7d
Mark Sobkow <mark.sobkow@gmail.com>
2026-08-25 04:23:55 -0600

    2026-08-25 04:10 CST I've once again eliminated the recursion and resulting stack overflow errors, but who knows how long that will be the case, because now I have some issues resolving IJavaReqAtomType and IJavaOptAtomType. Not sure what the root cause of that is yet.

--- src/main/java/server/markhome/mcf/v3_1/cfint/cfint/jpa/CFIntJpaLicenseFactoryService.java
+++ src/main/java/server/markhome/mcf/v3_1/cfint/cfint/jpa/CFIntJpaLicenseFactoryService.java
@@ -72,8 +72,7 @@ public class CFIntJpaLicenseFactoryService
 			mapped.setAuditClusterId(key.getAuditClusterId());
 			mapped.setAuditActionId(key.getAuditActionId());
 			mapped.setAuditSessionId(key.getAuditSessionId());
-			mapped.setAuditStamp(key.getAuditStamp());
-			mapped.setRequiredId( key.getRequiredId() );
+			mapped.setAuditStamp(key.getAuditStamp());$implBuffCopyKeyAttrToMapped$
 			return( mapped );
 		}
 	}
@@ -94,8 +93,7 @@ public class CFIntJpaLicenseFactoryService
 			return( (CFIntJpaLicenseByLicnTenantIdxKey)key );
 		}
 		else {
-			CFIntJpaLicenseByLicnTenantIdxKey mapped = new CFIntJpaLicenseByLicnTenantIdxKey();
-			mapped.setRequiredTenantId( key.getRequiredTenantId() );
+			CFIntJpaLicenseByLicnTenantIdxKey mapped = new CFIntJpaLicenseByLicnTenantIdxKey();$implBuffCopyKeyAttrToMapped$
 			return( mapped );
 		}
 	}
@@ -116,8 +114,7 @@ public class CFIntJpaLicenseFactoryService
 			return( (CFIntJpaLicenseByDomainIdxKey)key );
 		}
 		else {
-			CFIntJpaLicenseByDomainIdxKey mapped = new CFIntJpaLicenseByDomainIdxKey();
-			mapped.setRequiredTopDomainId( key.getRequiredTopDomainId() );
+			CFIntJpaLicenseByDomainIdxKey mapped = new CFIntJpaLicenseByDomainIdxKey();$implBuffCopyKeyAttrToMapped$
 			return( mapped );
 		}
 	}
@@ -138,9 +135,7 @@ public class CFIntJpaLicenseFactoryService
 			return( (CFIntJpaLicenseByUNameIdxKey)key );
 		}
 		else {
-			CFIntJpaLicenseByUNameIdxKey mapped = new CFIntJpaLicenseByUNameIdxKey();
-			mapped.setRequiredTopDomainId( key.getRequiredTopDomainId() );
-			mapped.setRequiredName( key.getRequiredName() );
+			CFIntJpaLicenseByUNameIdxKey mapped = new CFIntJpaLicenseByUNameIdxKey();$implBuffCopyKeyAttrToMapped$$implBuffCopyKeyAttrToMapped$
 			return( mapped );
 		}
 	}
